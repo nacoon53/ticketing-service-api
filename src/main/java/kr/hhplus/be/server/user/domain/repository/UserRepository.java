5937cefb0e3907bac947d635c5fab3f79b3c6780
nakyoung <nacoon53@gmail.com>
2025-01-10 02:16:11 +0900

    [#13][FEAT] 잔액충전, 조회 기능 구현

---
+++ src/main/java/kr/hhplus/be/server/user/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package kr.hhplus.be.server.user.domain.repository;
+
+import kr.hhplus.be.server.user.domain.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository {
+    Optional<User> findById(String userId);
+    User save(User user);
+}
