c61fdaebd5076a28a9b9bd9187cddb154d5c304d
nakyoung <nacoon53@gmail.com>
2025-01-10 03:58:51 +0900

    [#17][FEAT] 콘서트 유스케이스 개발

---
+++ src/main/java/kr/hhplus/be/server/concert/domain/usecase/ConcertUsecase.java
@@ -0,0 +1,17 @@
+package kr.hhplus.be.server.concert.domain.usecase;
+
+import kr.hhplus.be.server.concert.domain.entity.ConcertReservation;
+import kr.hhplus.be.server.concert.domain.entity.ConcertSeat;
+import kr.hhplus.be.server.concert.domain.entity.Concert;
+
+import java.util.List;
+
+public interface ConcertUsecase {
+    //콘서트 목록 조회(날짜 포함)
+    List<Concert> getConcertList();
+
+    //예약 가능한 좌석 조회
+    List<ConcertSeat> getAvailableSeat(long concertId);
+
+    ConcertReservation reserveSeat(String userId, long seatId, String token) throws Exception;
+}
