9f4161837c5f66cf305b3ce27bbb5d59315b251c
nakyoung <nacoon53@gmail.com>
2025-01-17 05:15:44 +0900

    [FEAT] 유저 잔액 기능 추가

--- src/main/java/kr/hhplus/be/server/module/user/presentation/dto/ChargeRequestDTO.java
+++ src/main/java/kr/hhplus/be/server/module/user/presentation/dto/ChargeRequestDTO.java
@@ -1,10 +1,5 @@
 package kr.hhplus.be.server.module.user.presentation.dto;
 
-import lombok.Builder;
-import lombok.Setter;
-
-@Builder
-@Setter
-public class ChargeRequestDTO {
-    private double amount;
-}
+public record ChargeRequestDTO (
+    double amount
+) {}
