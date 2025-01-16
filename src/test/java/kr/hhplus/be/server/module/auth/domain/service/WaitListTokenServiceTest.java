af02a55a55ea7e612ba93a31edfac4796faff9ff
nakyoung <nacoon53@gmail.com>
2025-01-17 05:16:04 +0900

    [TEST] 대기열 토큰 테스트 수정

--- src/test/java/kr/hhplus/be/server/module/auth/domain/service/WaitListTokenServiceTest.java
+++ src/test/java/kr/hhplus/be/server/module/auth/domain/service/WaitListTokenServiceTest.java
@@ -3,7 +3,7 @@ package kr.hhplus.be.server.module.auth.domain.service;
 import kr.hhplus.be.server.module.auth.domain.code.TokenStatus;
 import kr.hhplus.be.server.module.auth.domain.entity.WaitListToken;
 import kr.hhplus.be.server.module.auth.domain.repository.WaitListTokenRepository;
-import org.assertj.core.api.Assertions;
+import kr.hhplus.be.server.module.common.error.exception.ApiException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +14,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 
 @ExtendWith(MockitoExtension.class)
 class WaitListTokenServiceTest {
@@ -30,7 +35,7 @@ class WaitListTokenServiceTest {
     void init() {
         waitListToken = WaitListToken.builder()
                 .userId("test")
-                .token("test:1")
+                .token("test:712ab784-0cd2-4913-839a-5ca2ecb89943:1")
                 .status(TokenStatus.WAIT)
                 .build();
     }
@@ -46,46 +51,46 @@ class WaitListTokenServiceTest {
         WaitListToken generatedToken = waitListTokenService.generateWaitListToken(userId);
 
         // then
-        Assertions.assertThat(generatedToken).isNotNull();
-        Assertions.assertThat(generatedToken.getToken()).contains(userId); // 토큰에 userId가 포함되어야 함
-        Assertions.assertThat(generatedToken.getToken()).isEqualTo("test:2");
+        assertThat(generatedToken).isNotNull();
+        assertThat(generatedToken.getToken()).contains(userId); // 토큰에 userId가 포함되어야 함
+        assertThat(generatedToken.getToken()).matches(String.format("%s:[a-f0-9-]+:[0-9]+", userId));
     }
 
     @Test
     void 대기열_토큰_존재여부를_확인한다() {
         // given
         String userId = "test";
-        BDDMockito.given(waitListTokenRepository.findByUserId(userId)).willReturn(waitListToken);
+        BDDMockito.given(waitListTokenRepository.findTopByUserIdAndStatusNotOrderByLastIssuedAtAsc(anyString(), eq(TokenStatus.EXPIRED))).willReturn(waitListToken);
 
         // when
         boolean result = waitListTokenService.isExistWaitListTokenByUserId(userId);
 
         // then
-        Assertions.assertThat(result).isTrue();
+        assertThat(result).isTrue();
     }
 
     @Test
     void 토큰으로_대기열_토큰을_가져온다() throws Exception {
         // given
         String token = "test:1";
-        BDDMockito.given(waitListTokenRepository.findByToken(token)).willReturn(waitListToken);
+        BDDMockito.given(waitListTokenRepository.findByTokenWithLock(token)).willReturn(waitListToken);
 
         // when
         WaitListToken result = waitListTokenService.getWaitListTokenByToken(token);
 
         // then
-        Assertions.assertThat(result).isEqualTo(waitListToken);
+        assertThat(result).isEqualTo(waitListToken);
     }
 
     @Test
     void 없는_토큰으로_대기열_토큰_조회시_예외가_발생한다() {
         // given
         String token = "invalidToken";
-        BDDMockito.given(waitListTokenRepository.findByToken(token)).willReturn(null);
+        BDDMockito.given(waitListTokenRepository.findByTokenWithLock(token)).willReturn(null);
 
         // when & then
-        Assertions.assertThatThrownBy(() -> waitListTokenService.getWaitListTokenByToken(token))
-                .isInstanceOf(Exception.class)
+        assertThatThrownBy(() -> waitListTokenService.getWaitListTokenByToken(token))
+                .isInstanceOf(ApiException.class)
                 .hasMessage("토큰이 존재하지 않습니다. 새로 발급 받아 주세요.");
     }
 
@@ -98,7 +103,7 @@ class WaitListTokenServiceTest {
         int waitNumber = waitListTokenService.getWaitNumber(waitListToken);
 
         // then
-        Assertions.assertThat(waitNumber).isEqualTo(1);
+        assertThat(waitNumber).isEqualTo(1);
     }
 
 }
